cd585da5c1e5cf8a3c5291fcd75a5b807917e633
sudojan <jan.soedingrekso@tu-dortmund.de>
2019-06-21 16:30:45 -0400

    add possibility to pass input directory to further process a different simulation

--- simulation_scripts.py
+++ simulation_scripts.py
@@ -1,6 +1,7 @@
 import os
 import stat
 import string
+from glob import glob
 
 import click
 import yaml
@@ -35,6 +36,88 @@ def get_attribute_from_step(config, step, attibute_map):
 
     return KeyError('{} not defined in config'.format(attibute_map))
 
+def update_config_from_input_dir(cfg, input_dir):
+    input_dir = os.path.abspath(input_dir)
+    if not os.path.isdir(input_dir):
+        raise NameError('input folder {} is not a directory'.format(input_dir))
+    if input_dir.endswith('/'):
+        input_dir = input_dir[:-1]
+    config['input_folder'] = input_dir
+
+    file_list = glob(os.path.join(input_dir, '*.i3.bz2'))
+    nfiles = len(file_list)
+    if nfiles == 0:
+        raise ValueError('input directory ha no i3 files')
+    filename = file_list[0]
+    base_file_name = os.path.basename(filename)
+    dirname = os.path.dirname(filename)
+
+    #split base name to change the Level
+    split_base_name = base_file_name.split('_')
+    for idx, split_name in enumerate(split_base_name):
+        if split_name.startswith('Level'):
+            previous_step_level_name = split_base_name[idx]
+            split_base_name[idx] = '{step_level_name}'
+            break
+        else:
+            if idx == len(split_base_name)-1:
+                #reached end of file, no processing level in name
+                split_base_name = ['{step_level_name}'] + split_base_name
+    base_file_name = '_'.join(split_base_name)
+
+    # split again for dataset number and run number
+    split_base_name = base_file_name.split('.')
+    numbers_list = [stmp for stmp in split_base_name if stmp.isdigit() and len(stmp) == 6]
+    if len(numbers_list) != 2:
+        raise NameError('problems splitting file name {}'.format(tmp))
+    dataset_number = int(numbers_list[0])
+    # run_number = int(numbers_list[1])
+    idx_list = [split_base_name.index(tmp_num) for tmp_num in numbers_list]
+    # split_base_name[idx_list[0]] = '{dataset_number:6d}'
+    split_base_name[idx_list[1]] = '{run_number}'
+    base_file_name = '.'.join(split_base_name).replace(' ', '0')
+
+    run_numbers = []
+    # get run numbers
+    for idx, file_name in enumerate(file_list):
+        base_name = os.path.basename(file_name)
+        split_base_name = base_name.split('.')
+        run_numbers.append(int(split_base_name[idx_list[1]]))
+
+    # # extract run folder
+    # split_dir_name = dirname.split(os.sep)
+    # for dir_folder in split_dir_name[::-1]:
+    #     tmp_split = dir_folder.split('-')
+    #     if len(tmp_split) == 2:
+    #         if len(tmp_split[0]) == 5 and len(tmp_split[1]) == 5:
+    #             if tmp_split[0].isdigit() and tmp_split[1].isdigit():
+    #                 run_folder = dir_folder
+    #                 break
+
+    cfg.update({
+        'dataset_number': dataset_number,
+        'run_numbers': run_numbers,
+        # 'run_folder': run_folder,
+        })
+
+    # cfg['output_folder'] = STEP_FOLDER.format(**config)
+    # cfg['dataset_folder'] = DATASET_FOLDER.format(**config)
+    cfg['output_pattern'] = os.path.join('{run_folder}', base_file_name).format(**cfg)
+    # config['outfile_pattern'] = os.path.join(cfg['output_folder'], cfg['output_pattern'])
+
+    step_level_name = cfg['step_level_name']
+    cfg['step_level_name'] = previous_step_level_name
+    full_input_file = os.path.join(dirname, base_file_name).format(**cfg)
+    cfg['step_level_name'] = step_level_name
+
+    cfg.update({
+        'infile_pattern': full_input_file,
+        })
+
+    return cfg
+
+
+
 def create_filename(cfg, input=False):
     if input:
         step_name = cfg['step_name']
@@ -58,7 +141,8 @@ def create_filename(cfg, input=False):
 
 
 def write_job_files(config, step, check_existing=False,
-                    run_start=None, run_stop=None):
+                    run_start=None, run_stop=None,
+                    run_numbers_to_check=None):
     with open(config['job_template']) as f:
         template = f.read()
     output_base = os.path.join(config['processing_folder'], 'jobs')
@@ -72,19 +156,21 @@ def write_job_files(config, step, check_existing=False,
     scripts = []
     run_numbers = []
 
-    if run_start is None:
-        run_start = 0
-    else:
-        if run_start < 0 or run_start >= config['n_runs']:
-            raise ValueError('run_start is out of range: {!r}'.format(
-                                                                    run_start))
-    if run_stop is None:
-        run_stop = config['n_runs']
-    else:
-        if run_start >= run_stop or run_stop > config['n_runs']:
-            raise ValueError('run_stop is out of range: {!r}'.format(run_stop))
+    if run_numbers_to_check is None:
+        if run_start is None:
+            run_start = 0
+        else:
+            if run_start < 0 or run_start >= config['n_runs']:
+                raise ValueError('run_start is out of range: {!r}'.format(run_start))
+        if run_stop is None:
+            run_stop = config['n_runs']
+        else:
+            if run_start >= run_stop or run_stop > config['n_runs']:
+                raise ValueError('run_stop is out of range: {!r}'.format(run_stop))
 
-    for i in range(run_start, run_stop):
+        run_numbers_to_check = range(run_start, run_stop)
+
+    for i in run_numbers_to_check:
         config['run_number'] = i
         config['run_folder'] = get_run_folder(i)
         final_out = config['outfile_pattern'].format(**config)
@@ -112,7 +198,7 @@ def write_job_files(config, step, check_existing=False,
     return scripts, run_numbers
 
 
-def build_config(data_folder, custom_settings):
+def build_config(data_folder, custom_settings, input_folder_name=None):
     if data_folder is None:
         default = '/data/user/{}/simulation_scripts/'.format(getpass.getuser())
         data_folder = click.prompt(
@@ -129,7 +215,10 @@ def build_config(data_folder, custom_settings):
                    'run_number': '{run_number:6d}',
                    'run_folder': '{run_folder}'})
 
-    config['input_folder'] = PREVIOUS_STEP_FOLDER.format(**config)
+    if input_folder_name is None:
+        config['input_folder'] = PREVIOUS_STEP_FOLDER.format(**config)
+    else:
+        config = update_config_from_input_dir(config, input_folder_name)
     config['output_folder'] = STEP_FOLDER.format(**config)
     config['dataset_folder'] = DATASET_FOLDER.format(**config)
     config['processing_folder'] = PROCESSING_FOLDER.format(**config)
@@ -145,6 +234,8 @@ def build_config(data_folder, custom_settings):
 @click.argument('config_file', type=click.Path(exists=True))
 @click.option('--data_folder', '-d', default=None,
               help='folder were all files should be placed')
+@click.option('--input_folder_name', '-i', default=None,
+              help='folder were all files are read from')
 @click.option('--processing_scratch', '-p', default=None,
               help='Folder for the DAGMAN Files')
 @click.option('--dagman/--no-dagman', default=False,
@@ -160,6 +251,7 @@ def build_config(data_folder, custom_settings):
 @click.option('--run_stop', default=None, type=int,
               help='Only process runs up to this number.')
 def main(data_folder,
+         input_folder_name,
          config_file,
          processing_scratch,
          step,
@@ -207,8 +299,9 @@ def main(data_folder,
         if not os.path.isabs(job_template):
             job_template = os.path.join(SCRIPT_FOLDER, 'job_templates', job_template)
         custom_settings['job_template'] = job_template
-        config = build_config(data_folder, custom_settings)
-        config['infile_pattern'] = create_filename(config, input=True)
+        config = build_config(data_folder, custom_settings, input_folder_name)
+        if 'infile_pattern' not in config.keys():
+            config['infile_pattern'] = create_filename(config, input=True)
 
     config['processing_folder'] = PROCESSING_FOLDER.format(**config)
     config['outfile_pattern'] = create_filename(config)
@@ -232,10 +325,14 @@ def main(data_folder,
                 default=default)
         config['processing_scratch'] = os.path.abspath(processing_scratch)
 
+    prev_run_numbers = None
+    if 'run_numbers' in config.keys():
+        prev_run_numbers = config['run_numbers']
     script_files, run_numbers = write_job_files(config, step,
                                                 check_existing=resume,
                                                 run_start=run_start,
-                                                run_stop=run_stop)
+                                                run_stop=run_stop,
+                                                run_numbers_to_check=prev_run_numbers)
 
     if dagman or pbs:
         scratch_subfolder = '{dataset_number}_{step_name}'.format(**config)
