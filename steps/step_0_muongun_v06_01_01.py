f085d1cb2f57ea6d404b06339353f68c5d146c18
sudojan <jan.soedingrekso@tu-dortmund.de>
2019-06-26 14:50:48 -0400

    add simulation scripts for last release

---
+++ steps/step_0_muongun_v06_01_01.py
@@ -0,0 +1,6 @@
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v3.0.1/icetray-start
+#METAPROJECT simulation/V06-01-01
+from step_0_general_muongun import *
+
+if __name__ == '__main__':
+    main()
