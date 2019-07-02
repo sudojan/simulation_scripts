2babed9982bc8a87292d6548cf8b80536d8d628b
sudojan <jan.soedingrekso@tu-dortmund.de>
2019-07-02 00:23:45 -0400

    fall back to new reco software, but still using old spline tables

--- steps/step_5_pass2_muon_L3_v05_02_03.py
+++ steps/step_5_pass2_muon_L3_v05_02_03.py
@@ -1,5 +1,5 @@
-#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v3.0.1/icetray-start
-#METAPROJECT icerec/V05-02-03
+#!/bin/sh /cvmfs/icecube.opensciencegrid.org/py2-v3.1.1/icetray-start
+#METAPROJECT /data/user/jsoedingrekso/ic_software/combo_173346/build
 from step_5_pass2_muon_L3 import *
 
 if __name__ == '__main__':
